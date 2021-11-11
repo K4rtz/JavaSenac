39eb3d568db29b54ace786e7fa7caddfb736db2c
Administrador <Administrador@CAML192985>
2021-11-11 20:49:48 -0300

    Menu com While

---
+++ src/while02/While02.java
@@ -0,0 +1,31 @@
+
+package while02;
+
+import java.util.Scanner;
+
+
+public class While02 {
+
+    
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        
+        int opcao = 0;
+        
+        do {
+            System.out.println("Escolha uma opção:");
+            System.out.println("1- Iniciar jogo");
+            System.out.println("2- Ajuda");
+            System.out.println("3-Sair");
+            System.out.println("OPCÃO: ");
+            opcao = input.nextInt();
+        }while (opcao !=3);
+        System.out.println("Programa Finalizado");
+        
+        }
+                
+        
+    }
+    
+
